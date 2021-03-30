04453b08379913504f75a3a075d0c1eaa9686489
ranausama48 <ranausama48@gmail.com>
2021-03-31 01:26:28 +0500

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Body, Container, Content, Text, Header } from "native-base";
 
 export default function App() {
+  const [isReady, setIsReady] = React.useState(false);
+  React.useEffect(() => {
+    (async () => {
+      await Font.loadAsync({
+        Roboto: require("native-base/Fonts/Roboto.ttf"),
+        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+        ...Ionicons.font,
+      });
+      setIsReady(true);
+    })();
+  }, []);
+  if (!isReady) {
+    return <Text>Loading....</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Header>
+        <Body>
+          <Text>Home TOdo</Text>
+        </Body>
+      </Header>
+      <Content>
+        <Text>Home Tofo 2</Text>
+      </Content>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
